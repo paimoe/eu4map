1b29c3a2bffa9a33722d8dec1468b647ea46646e
paimoe <me@paimoe.com>
2018-05-15 19:49:35 +0800

    Add renamed achievements

--- renames.py
+++ renames.py
@@ -57,3 +57,20 @@ RELIGIONS = {
     'animism': 'animist',
     'buddhism': 'theravada',
 }
+
+# from achievements.txt: our converted name from the name in achievement yml file
+ACHIEVEMENT_KEYS = {
+    'a_heros_welcome': 'a_hero_s_welcome',
+    'take_that_habsburgs': 'take_that_von_habsburgs',
+    'guaranteer_of_peace': 'guarantor_of_peace',
+    'magellans_voyage': 'magellan_s_voyage',
+    'no_pirates_in_my_carribean': 'no_pirates_in_my_caribbean',
+    'dude_where_s_my_boat': 'dude_wheres_my_boat',
+    'komenoi_empire': 'komnenoi_empire',
+    'abu_bakr_IIs_ambition': 'abu_bakr_ii_s_ambition',
+    'the_burgundian_inheritance': 'the_burgundian_conquest',
+    'graze_my_horse_here_and_here': 'ill_graze_my_horse_here___and_here',
+    'three_trivial_tributaries': 'three_trivial_tributary_tribes',
+    'around_the_world_in_eighty_years': 'around_the_world_in_80_years',
+    'all_that_is_thine_shall_be_mine': 'all_thats_thine_shall_be_mine',
+}
